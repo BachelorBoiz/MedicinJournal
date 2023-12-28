88cbaef7455e1b8ba353d8872469bc8974b5ee6b
Mathias Kristensen <mathiaskristensen52@gmail.com>
2023-12-28 22:03:49 +0100

    Fix

--- MedicinJournal.Test/UnitTest1.cs
+++ MedicinJournal.Test/UnitTest1.cs
@@ -1,7 +1,10 @@
+using MedicinJournal.Security.Interfaces;
+
 namespace MedicinJournal.Test
 {
     public class UnitTest1
     {
+        private ISymmetricCryptographyService _service;
         [Fact]
         public void Test1()
         {
