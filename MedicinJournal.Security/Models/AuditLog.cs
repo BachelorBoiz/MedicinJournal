68352f861110a06ae1e8f64728c74767a63bfad1
LindholmGithub <71692428+LindholmGithub@users.noreply.github.com>
2023-12-30 19:17:01 +0100

    Update AuditLog.cs

--- MedicinJournal.Security/Models/AuditLog.cs
+++ MedicinJournal.Security/Models/AuditLog.cs
@@ -12,6 +12,7 @@ namespace MedicinJournal.Security.Models
         public string UserName { get; set; }
         public required string EntityName { get; set; }
         public required string Action { get; set; }
+        
         public DateTime Timestamp { get; set; }
         public required string Changes { get; set; }
     }
